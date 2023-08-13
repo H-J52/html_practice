647b234c61089ff9b43a2950c51aac97e12043e0
DoRaGee_ <122077890+H-J52@users.noreply.github.com>
2023-08-14 03:43:35 +0900

    Add files via upload

---
+++ html 연습/javascript/lib.js
@@ -0,0 +1,8 @@
+lib.js
+
+function over(obj) {
+    obj.src="C:/Users/ruy02/OneDrive/바탕 화면/media/banana.png";
+}
+function out(obj) {
+    obj.src="C:/Users/ruy02/OneDrive/바탕 화면/media/apple.png";
+}
