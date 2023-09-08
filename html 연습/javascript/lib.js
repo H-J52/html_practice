cc64d95e1eb34e28a23eccde871a1c0fceb82eb7
DoRaGee_ <122077890+H-J52@users.noreply.github.com>
2023-09-08 14:39:52 +0900

    Delete html 연습 directory

--- html 연습/javascript/lib.js
+++
@@ -1,8 +0,0 @@
-lib.js
-
-function over(obj) {
-    obj.src="C:/Users/ruy02/OneDrive/바탕 화면/media/banana.png";
-}
-function out(obj) {
-    obj.src="C:/Users/ruy02/OneDrive/바탕 화면/media/apple.png";
-}
